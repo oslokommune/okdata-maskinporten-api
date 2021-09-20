acd136c87a1cdd40175d3132aa5711eea5c2d5a1
Øyvind Nygard <oyvind.nygard@origo.oslo.kommune.no>
2021-09-20 13:36:08 +0200

    DP-2284: Add function for sending ssm parameter to other accounts (#15)
    * DP-2284: Add function for sending ssm parameter to other accounts
    
    * DP-2284: Reference role created with cloudformation
    
    * DP-2284: More descriptive role session name
    
    * DP-2284: Get region from environment
    
    * DP-2284: Rename arguments and add doc string
    
    * DP-2284: Update doc string
    
    * DP-2284: Use ssm name prefix
    
    * DP-2284: Remove reduntant variables
    
    * DP-2284: Put multiple parameters in same session
    
    * DP-2284: Return put_parameter responses
    
    * DP-2284: Add client_id to ssm name prefix
    
    * DP-2284: Use dataclasses.asdict
    
    * DP-2284: Remove yield
    
    * DP-2284: Pep-8/Pep-257
    
    * DP-2284: Fix backticks

--- maskinporten_api/ssm.py
+++ maskinporten_api/ssm.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass, asdict
 
 import boto3
 
@@ -8,3 +9,47 @@ def get_secret(key):
     client = boto3.client("ssm", region_name=os.environ["AWS_REGION"])
     response = client.get_parameter(Name=key, WithDecryption=True)
     return response["Parameter"]["Value"]
+
+
+@dataclass
+class MaskinportenSecrets:
+    keystore: str
+    key_id: str
+    key_password: str
+
+
+def send_secrets(
+    secrets: MaskinportenSecrets, maskinporten_client_id, destination_aws_account_id
+):
+    """Send secret values to another AWS Account.
+
+    Secret values are stored as SecureString SSM parameters with prefix
+    '/okdata/maskinporten/`maskinporten_client_id`/' in AWS account with ID
+    `destination_aws_account_id`.
+    """
+
+    sts_client = boto3.client("sts")
+    role_arn = (
+        f"arn:aws:iam::{destination_aws_account_id}:role/dataplatform-maskinporten"
+    )
+
+    assume_role_response = sts_client.assume_role(
+        RoleArn=role_arn, RoleSessionName="dataplatform-maskinporten"
+    )
+
+    credentials = assume_role_response["Credentials"]
+
+    ssm_client = boto3.client(
+        "ssm",
+        region_name=os.environ["AWS_REGION"],
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
+    )
+
+    for key, value in asdict(secrets):
+        ssm_client.put_parameter(
+            Name=f"/okdata/maskinporten/{maskinporten_client_id}/{key}",
+            Value=value,
+            Type="SecureString",
+        )
