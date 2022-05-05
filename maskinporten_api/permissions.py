73e79c8cd5a43c88e839936309a57ad16a7449be
Petter H. Juliussen <petter.juliussen@origo.oslo.kommune.no>
2022-05-05 09:56:53 +0200

    T#181 Check team membership and set team as resource owner

--- maskinporten_api/permissions.py
+++ maskinporten_api/permissions.py
@@ -5,24 +5,9 @@ import requests
 OKDATA_PERMISSION_API_URL = os.environ["OKDATA_PERMISSION_API_URL"]
 
 
-def create_okdata_permissions(
-    resource_name: str,
-    owner_principal_id: str,
-    auth_header: dict,
-):
-    # Determine caller user type. Allows Keycloak service accounts
-    # to create permissions for Maskinporten resources.
-    service_account_prefix = "service-account-"
-
-    if owner_principal_id.startswith(service_account_prefix):
-        user_id = owner_principal_id[len(service_account_prefix) :]
-        user_type = "client"
-    else:
-        user_type = "user"
-        user_id = owner_principal_id
-
+def create_okdata_permissions(resource_name, team_name, auth_header):
     create_permissions_body = {
-        "owner": {"user_id": user_id, "user_type": user_type},
+        "owner": {"user_id": team_name, "user_type": "team"},
         "resource_name": resource_name,
     }
 
@@ -37,8 +22,28 @@ def create_okdata_permissions(
 
 
 def get_user_permissions(bearer_token):
-    return requests.get(
+    res = requests.get(
         f"{OKDATA_PERMISSION_API_URL}/my_permissions",
         headers={"Authorization": f"Bearer {bearer_token}"},
         params={"resource_type": "maskinporten:client"},
-    ).json()
+    )
+    res.raise_for_status()
+    return res.json()
+
+
+def get_user_team(team_id, bearer_token, has_role=None):
+    params = {}
+
+    if has_role:
+        params["has_role"] = has_role
+
+    res = requests.get(
+        f"{OKDATA_PERMISSION_API_URL}/teams",
+        headers={"Authorization": f"Bearer {bearer_token}"},
+        params=params,
+    )
+    res.raise_for_status()
+    for team in res.json():
+        if team["id"] == team_id:
+            return team
+    return None
