649c182fba66125a1881e1cc022cb4193727489a
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2025-07-02 12:22:17 +0200

    Propagate validation errors from Digdir
    Propagate validation errors from Digdir's client and key creation
    endpoints, making it easier for us to troubleshoot the next time
    Digdir make changes to their validation.

--- resources/errors.py
+++ resources/errors.py
@@ -11,6 +11,19 @@ class ErrorResponse(Exception):
         self.message = message
 
 
+class DigdirValidationErrorResponse(ErrorResponse):
+    def __init__(self, response):
+        errors = response.json()["errors"]
+
+        super().__init__(
+            400,
+            "Validation error{} from Digdir's API:{}".format(
+                "s" if len(errors) > 1 else "",
+                "".join([f'\n- {e["errorMessage"]}' for e in errors]),
+            ),
+        )
+
+
 class Message(BaseModel):
     message: Optional[str]
 
